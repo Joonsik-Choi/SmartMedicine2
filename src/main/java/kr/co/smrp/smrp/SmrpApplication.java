f6c3f2e8516d4ff7ec2c52a88fe7e4c5527d4bda
wnstlr0615 <wnstlr0615@naver.com>
2020-09-23 01:45:43 +0900

    Init

---
+++ src/main/java/kr/co/smrp/smrp/SmrpApplication.java
@@ -0,0 +1,13 @@
+package kr.co.smrp.smrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmrpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmrpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/smrp/smrp/SmrpApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.smrp.smrp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmrpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
